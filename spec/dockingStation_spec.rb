75d5d59cdcb942df7a4341630437356cba94e835
CodeZeus-dev <constantine.akritides@gmail.com>
2020-10-27 16:56:51 +0000

    initialise a Bike object

--- spec/dockingStation_spec.rb
+++ spec/dockingStation_spec.rb
@@ -4,7 +4,7 @@ describe DockingStation do
   docking_station = described_class.new
 
   describe "#release" do
-    it "releases a bike" do
+    it "responds to release_bike method" do
       expect(docking_station.release_bike).to eq "Bike released"
     end
   end
