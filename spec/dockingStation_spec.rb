6692f5ce6b5b3e08ee1cc600dfe64b762ac5ebb1
CodeZeus-dev <constantine.akritides@gmail.com>
2020-10-28 17:03:29 +0000

    Optimised the dockingStation_spec.rb descriptions and examples, and implemented the working? test for the Bike class as an one-liner

--- spec/dockingStation_spec.rb
+++ spec/dockingStation_spec.rb
@@ -8,6 +8,11 @@ describe DockingStation do
 
   end
 
+  describe "#gets_bike" do
+    subject { :release_bike }
+    it { is_expected.not_to eq nil }
+  end
+
   describe "#working?" do
     it "responds to the working? method of the Bike class" do
       expect(subject.release_bike).to respond_to(:working?)
